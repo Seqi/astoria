53320f8c8a61c2468bc2b4f7b1a907a0fb84a4ff
seqi <tomseqi@live.co.uk>
2019-07-06 23:52:30 +0100

    Moved core code into one file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+let Poller = require('./poller')
+
 /**
  * Astoria options
  * @typedef {Object} options
- * @property {number} interval Interval between polling in seconds (min 30 secs)
+ * @property {number} interval Interval between polling in seconds (min 10 secs). Defaults to 30 seconds.
  */
 
 class Astoria {
@@ -9,11 +11,64 @@ class Astoria {
 	  * @param {options} options The configuration for Astoria
 	  */
 	constructor(options) {
-		if (options.interval < 10) {
+		if (options && options.interval < 10) {
 			throw new Error('Cannot set interval to less than 10 seconds to abide by 4chan API rules.')
 		}
 
-		this.options = options
+		this.options = {
+			interval: 30,
+			...options
+		}
+
+		this.poller = new Poller(options.interval)
+	}
+
+	/**
+	 * 
+	 * @param {string} board The 4chan board to target e.g. (v, ck, /biz/, /an/)
+	 * @return {Astoria} Current instance of Astoria
+	 */
+	board(board) {
+		this.board = board
+		return this
+	}
+
+	/**
+	 * 
+	 * @param {string} thread The thread number to target (e.g. 1234567)
+	 * @return {Astoria} Current instance of Astoria
+	 */
+	thread(thread) {
+		this.thread = thread
+		return this
+	}
+
+	/**
+	 * Callback containing Astoria instance with new threads/posts.
+	 * @callback onNewItems
+	 * @param {Astoria} astoria instance of Astoria with new items.
+	 * @param {int} newItems New items since last poll.
+	 */
+
+	/**
+	 * Begin listening to the specified board/thread. 
+	 * @param {onNewItems} callback 
+	 */
+	listen(callback) {
+		this.poller.onPoll(() => {
+			callback(this)
+		})
+
+		this.poller.poll()
+
+		return this
+	}
+
+	/**
+	 * Remove all listeners to this Astoria instance.
+	 */
+	stopListening() {
+		this.poller.cancel()
 	}
 }
 
